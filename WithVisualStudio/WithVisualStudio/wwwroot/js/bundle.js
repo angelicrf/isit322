706722c4a7c68975515bac79f53ab4e708e1777a
Your Name <you@example.com>
2020-04-30 15:21:10 -0700

    WMVCBegin

---
+++ WithVisualStudio/WithVisualStudio/wwwroot/js/bundle.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var element = document.createElement("p");
+    element.textContent = "This is the element from the JavaForth.js file";
+    document.querySelector("body").appendChild(element);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var element = document.createElement("p");
+    element.textContent = "This is the element from the (modified) JavaThird.js file";
+    document.querySelector("body").appendChild(element);
+});
